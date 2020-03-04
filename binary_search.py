54edcb843a9537588e24068b4ab33b3dedd00753
Stella <stelli@cmc.edu>
2020-03-04 15:53:17 -0800

    finishing

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,12 @@
 #!/bin/python3
-
 def find_smallest_positive(xs):
     '''
     Assume that xs is a list of numbers sorted from LOWEST to HIGHEST.
     Find the index of the smallest positive number.
     If no such index exists, return `None`.
-
-    HINT: 
+    HINT:
     This is essentially the binary search algorithm from class,
     but you're always searching for 0.
-
     >>> find_smallest_positive([-3, -2, -1, 0, 1, 2, 3])
     4
     >>> find_smallest_positive([1, 2, 3])
@@ -17,6 +14,41 @@ def find_smallest_positive(xs):
     >>> find_smallest_positive([-3, -2, -1]) is None
     True
     '''
+    #you are looking for the position of the index
+
+
+    left = 0
+    right = len(xs)-1
+
+    def go(left, right):
+        mid = (left+right)//2
+
+        if left == right:
+            if xs[mid] >0:
+                return mid
+            else:
+                return None
+
+        if 0<xs[mid]:
+            right = mid-1
+
+        if 0>xs[mid]:
+            left = mid+1
+
+        if 0==xs[mid]:
+            return mid+1
+        return go(left, right)
+
+    if len(xs) == 0:
+        return None
+    if len(xs)==1 and xs[i]<=0:
+        return None
+    if xs[0]>0:
+        return 0
+    else:
+        return go(left,right)
+    return go(left, right)
+
 
 
 def count_repeats(xs, x):
@@ -24,21 +56,62 @@ def count_repeats(xs, x):
     Assume that xs is a list of numbers sorted from HIGHEST to LOWEST,
     and that x is a number.
     Calculate the number of times that x occurs in xs.
-
-    HINT: 
+    HINT:
     Use the following three step procedure:
         1) use binary search to find the lowest index with a value >= x
         2) use binary search to find the lowest index with a value < x
         3) return the difference between step 1 and 2
-
     I highly recommend creating stand-alone functions for steps 1 and 2
     that you can test independently.
-
     >>> count_repeats([5, 4, 3, 3, 3, 3, 3, 3, 3, 2, 1], 3)
     7
-    >>> count_repeats([3, 2, 1], 4)
+    >>> count_repeats([3,2,1], 4)
     0
     '''
+
+    left = 0
+    right = len(xs) - 1
+
+    def step1(left,right):
+        mid = (left+right)//2
+        if x == xs[mid]:
+            if mid == 0 or xs[mid-1] > x:
+                return mid
+            else:
+                return step1 (left, mid-1) #searching forward
+            #searching for the big one that was ever there
+        if left == right:
+            return None #search ended
+        if x < xs[mid]:
+            return step1 (mid+1,right) # from a sequence that's from big to small, you move the boundary to right
+        if x > xs[mid]:
+            return step1 (left, mid-1) #you move the boundary to the right to find if the bigger numbers would match
+
+    def step2(left,right):
+        mid = (left+right)//2
+        if x == xs[mid]:
+            if mid == len(xs)-1 or xs[mid+1] < x: #till the last one
+                return mid
+            else:
+                return step2 (mid+1, right)
+        if left == right:
+            return None
+        if x < xs[mid]:
+            return step2(mid+1,right) #searching backwards
+        if x > xs[mid]:
+            return step2(left, mid-1)
+
+    if len(xs) == 0:
+        return 0
+
+    a = step1(left, right)
+    b = step2(left, right)
+
+    if a == None or b == None:
+        return 0
+    else:
+        return b-a+1
+
 
 
 def argmin(f, lo, hi, epsilon=1e-3):
@@ -46,19 +119,31 @@ def argmin(f, lo, hi, epsilon=1e-3):
     Assumes that f is an input function that takes a float as input and returns a float with a unique global minimum,
     and that lo and hi are both floats satisfying lo < hi.
     Returns a number that is within epsilon of the value that minimizes f(x) over the interval [lo,hi]
-
     HINT:
     The basic algorithm is:
         1) The base case is when hi-lo < epsilon
         2) For each recursive call:
             a) select two points m1 and m2 that are between lo and hi
             b) one of the 4 points (lo,m1,m2,hi) must be the smallest;
-               depending on which one is the smallest, 
+               depending on which one is the smallest,
                you recursively call your function on the interval [lo,m2] or [m1,hi]
-
     >>> argmin(lambda x: (x-5)**2, -20, 20)
     5.000040370009773
     >>> argmin(lambda x: (x-5)**2, -20, 0)
     -0.00016935087808430278
     '''
+    left=lo
+    right = hi
 
+    def go(left,right):
+        m1 = left + (right-left)/4
+        m2 = left + (right-left)/1.33333
+        if right-left < epsilon:
+            return right
+        if f(m1) > f(m2):
+            left=m1
+            return go(left,right)
+        if f(m1) < f(m2):
+            right =m2
+            return go(left,right)
+    return go(left, right)
